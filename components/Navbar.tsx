3fd60ef73e0c04b2046941584924b155ffea2c4d
Yasser Imzi <imziyasser00@gmail.com>
2024-09-04 20:04:09 +0200

    done

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full p-8 flex items-center'>
+      <Image 
+       width={400}
+       height={400}
+      />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
